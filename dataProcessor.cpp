7bfebaa4153190d47787fef11eeb5158b2eb2fe0
AMIRIOX <wumingyun2120@outlook.com>
2020-11-22 09:07:24 +0800

    [feature] Beautify the display status

--- dataProcessor.cpp
+++ dataProcessor.cpp
@@ -22,36 +22,50 @@ void DataProcessor::showSingleStatus(string name) {
   }
   double percent = (double(readval)/100.0);
   string nameOut = string("➜")+name+string(":");
-  printf("\033[1;33;34m%s\033[0m",nameOut.c_str());
   if(name=="contribution") {
-    printf("\033[4;37m %d\n\033[0m", readval);
+    printf("\033[1;33;34m%55s \033[0m",nameOut.c_str());
+    printf("\033[4;37m%d\n\033[0m", readval);
     return;
   }
+  printf("\033[1;33;34m%55s\n\033[0m",nameOut.c_str());
   cout << "[";
   double needOutNum = STATUS_BAR_LENGTH*percent;
   if(percent>1) needOutNum=STATUS_BAR_LENGTH;
 
   for(int i=1;i<=needOutNum;i++) {
-    if(percent<0 || (percent>=0 && percent<0.5)) {
-      cout << "\033[8;37;41m█\033[0m";
-    }else if(percent>=0.5 && percent<1) {
-      cout << "\033[8;37;44m█\033[0m";
-    }else if(percent==1) {
-      cout << "\033[8;37;42m█\033[0m";
-    }else if(percent>1) {
-      cout << "\033[8;37;45m█\033[0m";
+    if(name=="tiring") {
+      if(percent<0 || (percent>=0 && percent<0.1)) {
+        cout << "\033[8;37;45m█\033[0m";    //purple
+      }else if(percent>=0.1 && percent<=0.4) {
+        cout << "\033[8;37;42m█\033[0m";    //green
+      }else if(percent>0.4 && percent<=0.75) {
+        cout << "\033[8;37;44m█\033[0m";    //blue
+      }else if(percent>0.75) {
+        cout << "\033[8;37;41m█\033[0m";    //red
+      }
+    }else {
+      if(percent<0 || (percent>=0 && percent<0.5)) {
+        cout << "\033[8;37;41m█\033[0m";
+      }else if(percent>=0.5 && percent<1) {
+        cout << "\033[8;37;44m█\033[0m";
+      }else if(percent==1) {
+        cout << "\033[8;37;42m█\033[0m";
+      }else if(percent>1) {
+        cout << "\033[8;37;45m█\033[0m";
+      }
     }
   }
   for(int i=1;i<=STATUS_BAR_LENGTH-needOutNum;i++) {
     cout << "\033[8;37;37m \033[0m";
   }
   cout << "]";
-  printf("\033[4;37m%d \033[0m", readval);
+  printf("\033[4;37m%d\033[0m", readval);
+  cout << " ";
   printf("\033[5;37m%f%\033[0m", percent*100);
   cout << endl;
 }
 void DataProcessor::showStatus() {
-  cout << "The Wing Master Lab Result Today" << endl;
+  printf("%55s","The Wing Master Lab Result Today\n");
   showSingleStatus("contribution");
   showSingleStatus("contritoday");
   showSingleStatus("tiring");
